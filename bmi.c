8ea4e185e07b0f3e0fc6e0e1ce606dd38f568c24
LeonCarrizo403461 <leoncarrinu10@gmail.com>
2025-06-22 20:35:18 -0300

    Ingresa cambios solicitados del tp5
    Modifica el programa tP3 para que no se permita el ingreso de números negativos, mostrando un mensaje y volviendo a preguntar.

--- bmi.c
+++ bmi.c
@@ -2,33 +2,41 @@
 
 int main()
 { //Leon Carrizo Nuñez
-    float altura;
-    float peso; 
-    float imc;
+float altura, peso, imc;
+
+	printf("Ingrese su altura en metros: \n");
+      	scanf("%f",&altura);
+
+	while (altura <= 0) {
+		printf("ERROR. Ingrese una altura mayor que cero: ");
+		scanf("%f", &altura);
+	}
+	printf("Ingrese su peso en kg: \n");
+      	scanf(" %f",&peso);
+
+	while (peso <= 0) {
+		printf("ERROR. Ingrese un peso mayor que cero: ");
+		scanf("%f", &peso);
+	}
+     	imc = peso / (altura * altura);
+	printf("Su indice de masa corporal es: %f \n\n", imc);
+      	printf("Indice| Condicion\n-----------------------------\n<18.5 | Bajo peso\n18.5 a 24.9 | Normal\n25.0 a 29.9 | Sobrepeso\n>=30| Obesidad\n");
     
-     printf("Ingrese su altura en metros: \n");
-      scanf("%f",&altura);
-     printf("Ingrese su peso en kg: \n");
-      scanf(" %f",&peso);
-     imc = peso / (altura * altura);
-     printf("Su indice de masa corporal es: %f \n\n", imc);
-     printf("‭Indice| Condicion‬\n-----------------------------‬\n<18.5 | Bajo peso‬\n18.5 a 24.9 | Normal‬\n25.0 a 29.9 | Sobrepeso‬\n>=30| Obesidad\n");
-    
-	if (imc >= 30)
-	{
-		printf("Su condicion es: Obesidad");
-	}
-	else if (imc < 30 && imc >= 25)
-	{
-		printf("Su condicion es: Sobrepeso");
-	}
-	else if (imc < 25 && imc >= 18.5)
-	{
-		printf("Su condicion es: Normal");
-	}
-	else
-	{
-		printf("Su condicion es: Bajo peso");
-	}
-    return 0;
+		if (imc >= 30)
+		{
+			printf("Su condicion es: Obesidad");
+		}
+		else if (imc < 30 && imc >= 25)
+		{
+			printf("Su condicion es: Sobrepeso");
+		}
+		else if (imc < 25 && imc >= 18.5)
+		{
+			printf("Su condicion es: Normal");
+		}
+		else
+		{
+			printf("Su condicion es: Bajo peso");
+		}
+return 0;
 }
